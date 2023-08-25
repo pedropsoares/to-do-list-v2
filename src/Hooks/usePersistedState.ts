c1b988be6c8a9ca06338878b2a5e4588e050484b
Pedro Soares <pedropaulos.1998@gmail.com>
2023-08-25 09:24:07 -0300

    more generalist semantics implemented

--- src/Hooks/usePersistedState.ts
+++ src/Hooks/usePersistedState.ts
@@ -2,8 +2,8 @@ import { useCallback, useState } from "react"
 
 const usePersistedState = <T>(key: string, initialValue: T) => {
   const [storedValue, setStoredValue] = useState<T>(() => {
-    const tasksStorage = localStorage.getItem(key)
-    return tasksStorage ? JSON.parse(tasksStorage) : initialValue
+    const item = localStorage.getItem(key)
+    return item ? JSON.parse(item) : initialValue
   })
 
   const setValue = useCallback(
